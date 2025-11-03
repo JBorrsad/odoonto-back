caa51ea849d5278f8011c7afa7d4b5741039ef23
JBorrsad <149406696+JBorrsad@users.noreply.github.com>
2025-11-03 19:48:03 +0100

    fix del data seeder y del generador de diagramas

--- src/main/java/odoonto/infrastructure/testing/DataSeeder.java
+++ src/main/java/odoonto/infrastructure/testing/DataSeeder.java
@@ -35,7 +35,6 @@ import java.util.List;
 import java.util.Random;
 import java.util.Set;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 // Importar Firestore para limpieza directa
 import com.google.cloud.firestore.Firestore;
